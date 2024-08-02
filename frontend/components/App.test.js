d1c24a96bcb4de9ce92f4e5927e41020a3a66ef6
Stanley kemboi <kstan8482@gmail.com>
2024-08-02 02:26:04 -0500

    wip

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,7 +1,131 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import App from './App'
+// import React from 'react'
+// import { render, screen } from '@testing-library/react'
+// import '@testing-library/jest-dom'
+// import App from './App'
+// import txt from '../i18n/index.json'
+
+// describe('Module 4 Project Tests', () => {
+//   describe('English Language', () => {
+//     /*
+//       👉 TASK 1
+
+//       One test is done for you as an example.
+//     */
+//       let texts = getEntriesByKeyPrefix(text.en, 'TEXT')
+//       for ( let [key, val] of texts) {
+//         test(`${key} is visible`, () => {
+//           render(<App />)
+//           expect(screen.getByText(val)).toBeVisible()
+//       })     
+//     }
+//     let labels = getEntriesByKeyPrefix(text.en, 'LABEL')
+//       for ( let [key, val] of labels) {
+//         test(`${key} is visible`, () => {
+//           render(<App />)
+//           expect(screen.getByLabelText(val)).toBeVisible()
+//         })
+//       }
+//     test(`PLACEHOLDER_USERNAME is visible`, () => {
+//       render(<App lang="en" />)
+//       expect(screen.getByPlaceholderText(txt.en.PLACEHOLDER_USERNAME)).toBeVisible()
+//     })
+//   })
+//   describe('Spanish Language', () => {
+//     let texts = getEntriesByKeyPrefix(text.esp, 'TEXT')
+//       for ( let [key, val] of texts) {
+//         test(`${key} is visible`, () => {
+//           render(<App />)
+//           expect(screen.getByText(val)).toBeVisible()
+//       })     
+//     }
+    
+//     let labels = getEntriesByKeyPrefix(text.esp, 'LABEL')
+//       for ( let [key, val] of labels) {
+//         test(`${key} is visible`, () => {
+//           render(<App />)
+//           expect(screen.getByLabelText(val)).toBeVisible()
+//       })     
+//     }    
+//     test(`PLACEHOLDER_USERNAME is visible`, () => {
+//       render(<App lang="esp" />)
+//       expect(screen.getByPlaceholderText(txt.esp.PLACEHOLDER_USERNAME)).toBeVisible()
+//     })
+//     /*
+//       👉 TASK 3
+
+//       This is done after making the UI multilingual.
+//     */
+//   })
+//   describe('getEntriesByKeyPrefix', () => {
+//     test('can extract the correct data', () => {
+//       const obj = {
+//         abc_1: "data_abc_1",
+//         abc_2: "data_abc_2",
+//         xyz_1: "data_xyz_1",
+//         abc_3: "data_abc_3",
+//     }
+//     const expected = [      
+
+//         ["abc_1", "data_abc_1"],
+//         ["abc_2", "data_abc_2"],
+//         ["abc_3", "data_abc_3"],
+      
+//       ]
+//   } 
+//    const expected2 = [
+//     ["xyz_1", "data_xyz_1"],
+//   ]
+//   expect(getEntriesByKeyPrefix(obj, "abc")).toEqual(expected)
+//   expect(getEntriesByKeyPrefix(obj, "xyz")).toEqual(expected2)
+//   expect(getEntriesByKeyPrefix(obj, "Bruh")).toEqual([])
+//     })
+//   }) 
+// })
+// function getEntriesByKeyPrefix(obj, keyPrefix) {
+//   return Object.entries(obj).filter(([key]) => key.split("_")[0] === keyPrefix) 
+ 
+  
+//   /*
+//     👉 TASK 4 part 1
+
+//     Implement a function that takes as first argument an object `obj` such as this:
+
+//     {
+//       abc_1: "data_abc_1",
+//       abc_2: "data_abc_2",
+//       xyz_1: "data_xyz_1",
+//       abc_3: "data_abc_3",
+//     }
+
+//     and takes as second argument a string `keyPrefix` such as this: "abc"
+
+//     and returns an array of arrays such as this (for the arguments given in the examples above):
+
+//     [
+//       ["abc_1", "data_abc_1"],
+//       ["abc_2", "data_abc_2"],
+//       ["abc_3", "data_abc_3"],
+//     ]
+
+//     If the function is passed the same `obj` as above but a `keyPrefix` of "xyz" then it would return:
+
+//     [
+//       ["xyz_1", "data_xyz_1"],
+//     ]
+
+//     If the function is passed the same `obj` as above but a `keyPrefix` of "foo" then it would return the empty array.
+
+//     The function looks inside the object `obj`, finds all properties whose property names begin
+//     with the `keyPrefix` given (followed by an underscore), and reorganizes the information before returning it.
+//     The properties that match the `keyPrefix` are returned inside an array holding key-value-pair sub-arrays.
+
+//   */
+// }
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+import txt from '../i18n/index.json';
 
 describe('Module 4 Project Tests', () => {
   describe('English Language', () => {
@@ -10,68 +134,76 @@ describe('Module 4 Project Tests', () => {
 
       One test is done for you as an example.
     */
-    test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
-      render(<App lang="en" />)
-      expect(screen.getByText("Create an Account")).toBeVisible()
-    })
-  })
-  describe('Spanish Language', () => {
-    /*
-      👉 TASK 3
-
-      This is done after making the UI multilingual.
-    */
-  })
-  describe('getEntriesByKeyPrefix', () => {
-    test('can extract the correct data', () => {
-    /*
-      👉 TASK 4 part 2
-
-      Implement the function `getEntriesByKeyPrefix` below
-      and then come back here and write a few tests
-      to ensure it works as expected.
-
-      Although it should be noted that commonly,
-      the tests are written _before_ implementing
-      the function being tested.
-    */
-    })
-  })
-})
-function getEntriesByKeyPrefix(obj, keyPrefix) {
-  /*
-    👉 TASK 4 part 1
-
-    Implement a function that takes as first argument an object `obj` such as this:
-
-    {
-      abc_1: "data_abc_1",
-      abc_2: "data_abc_2",
-      xyz_1: "data_xyz_1",
-      abc_3: "data_abc_3",
+    let texts = getEntriesByKeyPrefix(txt.en, 'TEXT');
+    for (let [key, val] of texts) {
+      test(`${key} is visible`, () => {
+        render(<App />);
+        expect(screen.getByText(val)).toBeVisible();
+      });
     }
 
-    and takes as second argument a string `keyPrefix` such as this: "abc"
+    let labels = getEntriesByKeyPrefix(txt.en, 'LABEL');
+    for (let [key, val] of labels) {
+      test(`${key} is visible`, () => {
+        render(<App />);
+        expect(screen.getByLabelText(val)).toBeVisible();
+      });
+    }
 
-    and returns an array of arrays such as this (for the arguments given in the examples above):
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="en" />);
+      expect(screen.getByPlaceholderText(txt.en.PLACEHOLDER_USERNAME)).toBeVisible();
+    });
+  });
 
-    [
-      ["abc_1", "data_abc_1"],
-      ["abc_2", "data_abc_2"],
-      ["abc_3", "data_abc_3"],
-    ]
+  describe('Spanish Language', () => {
+    let texts = getEntriesByKeyPrefix(txt.esp, 'TEXT');
+    for (let [key, val] of texts) {
+      test(`${key} is visible`, () => {
+        render(<App lang="esp" />);
+        expect(screen.getByText(val)).toBeVisible();
+      });
+    }
 
-    If the function is passed the same `obj` as above but a `keyPrefix` of "xyz" then it would return:
+    let labels = getEntriesByKeyPrefix(txt.esp, 'LABEL');
+    for (let [key, val] of labels) {
+      test(`${key} is visible`, () => {
+        render(<App lang="esp" />);
+        expect(screen.getByLabelText(val)).toBeVisible();
+      });
+    }
 
-    [
-      ["xyz_1", "data_xyz_1"],
-    ]
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByPlaceholderText(txt.esp.PLACEHOLDER_USERNAME)).toBeVisible();
+    });
+  });
 
-    If the function is passed the same `obj` as above but a `keyPrefix` of "foo" then it would return the empty array.
+  describe('getEntriesByKeyPrefix', () => {
+    test('can extract the correct data', () => {
+      const obj = {
+        abc_1: "data_abc_1",
+        abc_2: "data_abc_2",
+        xyz_1: "data_xyz_1",
+        abc_3: "data_abc_3",
+      };
+      const expected = [
+        ["abc_1", "data_abc_1"],
+        ["abc_2", "data_abc_2"],
+        ["abc_3", "data_abc_3"],
+      ];
+      const expected2 = [
+        ["xyz_1", "data_xyz_1"],
+      ];
 
-    The function looks inside the object `obj`, finds all properties whose property names begin
-    with the `keyPrefix` given (followed by an underscore), and reorganizes the information before returning it.
-    The properties that match the `keyPrefix` are returned inside an array holding key-value-pair sub-arrays.
+      expect(getEntriesByKeyPrefix(obj, "abc")).toEqual(expected);
+      expect(getEntriesByKeyPrefix(obj, "xyz")).toEqual(expected2);
+      expect(getEntriesByKeyPrefix(obj, "Bruh")).toEqual([]);
+    });
+  });
+});
 
-  */
+function getEntriesByKeyPrefix(obj, keyPrefix) {
+  return Object.entries(obj).filter(([key]) => key.startsWith(keyPrefix + "_"));
 }
+
